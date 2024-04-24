c7b1cd69b40a754a8314ed4ae5c247a317e343d2
Mathys <penson.mathys@gmail.com>
2024-04-24 19:54:06 +0200

    Fixing PWA

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('my-cache').then(cache => {
             return cache.addAll([
-                '/samenvattingen/',
-                '/samenvattingen/index.html',
-                '/samenvattingen/styles.css',
-                '/samenvattingen/icon.ico'
+                '/Samenvattingen/',
+                '/Samenvattingen/index.html',
+                '/Samenvattingen/styles.css',
+                '/Samenvattingen/icon.ico'
             ]);
         })
     );
